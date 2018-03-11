a7b32920fd61fa5fc7f12db355c21f86d4065975
root <MarisaG@bbslist.synchronetbbs.org>
2018-03-11 21:46:09 +0000

    Now allows status messages to be sent to BBS owner if site is unreachable and this is desired...

--- scan_bbs_list.pl
+++ scan_bbs_list.pl
@@ -7,6 +7,9 @@ use strict;
 use warnings;
 use IO::Socket::PortState qw(check_ports);
 use DBI;
+use Email::Simple;
+use Email::Simple::Creator;
+use Email::Sender::Simple qw(sendmail);
 
 # No changes below here
 my $CurHost="";
@@ -15,7 +18,6 @@ my $CurId=0;
 my $CurStatus="";
 my $timeout=5;
 my $VERSION="1.0";
-my $UpOrDown="";
 my $DB_Owner="";
 my $DB_Pswd="";
 my $DB_Name="";
@@ -23,6 +25,9 @@ my $DB_Prefix="";
 my $DB_Table="";
 my $dbh;
 my $CONF_FILE="/root/bbs-check-status/config.ini";
+my $EMAIL_SUBJ="";
+my $EMAIL_FROM="";
+my $CurNotify="";
 
 # Read in configuration options
 open(CONF, "<$CONF_FILE") || die("Unable to read config file '$CONF_FILE'");
@@ -47,8 +52,27 @@ while(<CONF>)
 	{
 		$DB_Prefix = $FIELD_VALUE;
 	}
+	elsif ($FIELD_TYPE eq "Email_Subj")
+	{
+		$EMAIL_SUBJ = $FIELD_VALUE;
+	}
+	elsif ($FIELD_TYPE eq "Email_From")
+	{
+		$EMAIL_FROM = $FIELD_VALUE;
+	}
 }
 close(CONF);
+
+if ($EMAIL_SUBJ eq "")
+{
+	print "You have not set a email subject in $CONF_FILE\n";
+	exit 1;
+}
+if ($EMAIL_FROM eq "")
+{
+	print "You have not set a email sender in $CONF_FILE\n";
+	exit 1;
+}
 
 # Marks the BBS state and check time
 sub MarkBBS
@@ -66,6 +90,28 @@ sub MarkBBS
 		$timeString,
 		$CurStatus,
 		$CurId);
+	# Should we send owner a note?
+	if (($CurStatus eq "Unreachable") && ($CurNotify ne ""))
+	{
+		my $CurBody = <<"END_MESSAGE_BODY";
+Dear $CurNotify,
+ 
+At our last scan of your BBS we could not connect to it. You may want to look into it or disable notifications if you don't want to get these messages in the future.
+
+Next check will be in roughly 6 hours.
+ 
+Regards,
+The Admins at BBS List @ SynchronetBBS.org
+END_MESSAGE_BODY
+		my $email = Email::Simple->create(
+		header => [
+		       From => $EMAIL_FROM,
+		       To => $CurNotify,
+		       Subject => $EMAIL_SUBJ,
+		]
+		body => $CurBody);
+		sendmail($email);
+	}
 }
 
 # Checks to see if the BBS is up
@@ -95,10 +141,8 @@ $dbh = DBI->connect ("DBI:mysql:database=$DB_Name:host=localhost",
 
 $DB_Table = $DB_Prefix . "jvld_links";
 
-#print ("Table is '$DB_Table'\n");
-
 ### The statement handle
-my $sth = $dbh->prepare("SELECT id, partner_url, field1, field2 FROM $DB_Table");
+my $sth = $dbh->prepare("SELECT id, partner_url, field1, field2, field11 FROM $DB_Table");
 
 $sth->execute or die $dbh->errstr;
 
@@ -109,6 +153,7 @@ while (my $row = $sth->fetchrow_hashref)
 	$CurId = $row->{'id'};
 	$CurHost = $row->{'field1'};
 	$CurPort = $row->{'field2'};
+	$CurNotify = $row->{'field11'};
 	CheckBBS();
 }
 
